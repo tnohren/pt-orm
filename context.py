d6bcd30e4790e743056e97d31efc5a2ab76b7083
Trey Nohren <twnohren@ucdavis.edu>
2025-05-10 14:12:43 -0700

    Add table alters

--- context.py
+++ context.py
@@ -187,3 +187,104 @@ class Context:
 
         # Run create table script.
         Context._run(query)
+
+    def table_alters(self):
+        """
+        Alter tables.
+        """
+
+        # Get current column names in DB. To be used in determining column additions.
+        model_columns = self.get_column_names()
+        db_columns_query = f"""
+        SELECT column_name, character_maximum_length
+        FROM information_schema.columns
+        WHERE table_name = '{self._model.name}'
+        """
+        if self._model.schema:
+            db_columns_query += f"\nAND table_schema = '{self._model.schema}'"
+        db_columns = {
+            column[0]: column[1] \
+            for column in Context._run(db_columns_query, include_results=True)
+        }
+
+        # Detect table alters.
+        column_addition_scripts: list = []
+        column_drop_scripts: list = []
+        column_length_change_scripts: list = []
+        column_rename_scripts: list = []
+        old_column_names: list[str] = []
+        for model_column in model_columns:
+
+            # Add column renames.
+            original_name = getattr(self._model, model_column).original_name
+            is_renamed: bool = False
+            if original_name is not None and len(original_name) > 0:
+                get_column_query = f"""
+                SELECT column_name
+                FROM information_schema.columns
+                WHERE table_name = '{self._model.name}'
+                AND column_name = '{original_name}
+                """
+                if self._model.schema is not None and len(self._model.schema) > 0:
+                    get_column_query += f"\nAND table_schema = '{self._model.schema}'"
+                if Context._run(get_column_query, include_results=True):
+                    old_column_names.append(original_name)
+                    is_renamed = True
+                    column_rename_scripts.append(f"RENAME COLUMN {original_name} TO {model_column}")
+
+            # Add column additions.
+            if not is_renamed and model_column not in db_columns:
+                column_addition_scripts.append(
+                    f"ADD COLUMN {model_column} {getattr(self._model, model_column).db_data_type}"
+                )
+
+        # Add column drops.
+        for db_column in db_columns:
+            if not hasattr(self._model, db_column) \
+            and db_column not in old_column_names:
+                column_drop_scripts.append(f"DROP COLUMN {db_column}")
+
+        # Add VARCHAR length updates.
+        for model_column in model_columns:
+            new_length = getattr(self._model, model_column).length
+            if new_length:
+                if model_column in db_columns \
+                and db_columns[model_column] != new_length:
+                    column_length_change_scripts.append(
+                        f"ALTER COLUMN {model_column} TYPE VARCHAR({new_length})"
+                    )
+                else:
+                    original_name = getattr(self._model, model_column).original_name
+                    if original_name is not None and len(original_name) > 0 \
+                    and original_name in db_columns \
+                    and db_columns[original_name] != new_length:
+                        column_length_change_scripts.append(
+                            f"ALTER COLUMN {model_column} TYPE VARCHAR({new_length})"
+                        )
+
+        all_queries = []
+
+        # Run column renames.
+        for column_rename in column_rename_scripts:
+            all_queries.append(f"ALTER TABLE {self.get_table_name()} {column_rename}")
+
+        # Run column additions.
+        if len(column_addition_scripts) > 0:
+            column_addition_query = f"ALTER TABLE {self.get_table_name()}\n"
+            column_addition_query += ",\n".join(column_addition_scripts)
+            all_queries.append(column_addition_query)
+
+        # Run column drops.
+        if len(column_drop_scripts) > 0:
+            column_drop_query = f"ALTER TABLE {self._get_table_name()}\n"
+            column_drop_query += ",\n".join(column_drop_scripts)
+            all_queries.append(column_drop_scripts)
+
+        # Run VARCHAR length updates.
+        if len(column_length_change_scripts) > 0:
+            column_length_change_query = f"ALTER TABLE {self._get_table_name()}\n"
+            column_length_change_query += ",\n".join(column_length_change_scripts)
+            all_queries.append(column_length_change_query)
+
+        for query in all_queries:
+            Context._run(query)
